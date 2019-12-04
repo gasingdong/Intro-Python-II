0211fbbb36084140194383063b13e4091b66761d
Brandon Dong <gasingdong@gmail.com>
2019-12-04 12:49:24 -0800

    extract room setup to map class

--- src/adv.py
+++ src/adv.py
@@ -2,63 +2,8 @@ import textwrap
 from room import Room
 from item import Item
 from player import Player
+from map import Map
 from command import *
-
-# Declare all the rooms
-
-rooms = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons."),
-
-    'foyer':    Room("Foyer",
-                     ("Dim light filters in from the south. Dusty passages " +
-                      "run north and east.")),
-
-    'overlook': Room("Grand Overlook",
-                     ("A steep cliff appears before you, falling into the " +
-                      "darkness. Ahead to the north, a light flickers in " +
-                      "the distance, but there is no way across the chasm.")),
-
-    'narrow':   Room("Narrow Passage",
-                     "The narrow passage bends here from west to north. The " +
-                     "smell of gold permeates the air."),
-
-    'treasure': Room("Treasure Chamber",
-                     "You've found the long-lost treasure chamber! Sadly, " +
-                     "it has already been completely emptied by earlier " +
-                     "adventurers. The only exit is to the south."),
-}
-
-
-# Link rooms together
-
-rooms['outside'].n_to = rooms['foyer']
-rooms['foyer'].s_to = rooms['outside']
-rooms['foyer'].n_to = rooms['overlook']
-rooms['foyer'].e_to = rooms['narrow']
-rooms['overlook'].s_to = rooms['foyer']
-rooms['narrow'].w_to = rooms['foyer']
-rooms['narrow'].n_to = rooms['treasure']
-rooms['treasure'].s_to = rooms['narrow']
-
-rooms['outside'].add_item(Item("lantern", "a lantern with oil"))
-
-#
-# Main
-#
-
-# Make a new player object that is currently in the 'outside' room.
-
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
 
 commands = [
     QuitCommand("Quit", "q", "quit"),
@@ -70,7 +15,10 @@ commands = [
     HelpCommand("Help", "h", "help"),
 ]
 
-player = Player("player", rooms['outside'])
+map = Map()
+map.setup()
+
+player = Player("player", map.get_starting_room())
 print("")
 player.current_room.get_scene()
 
---
+++ src/map.py
@@ -0,0 +1,49 @@
+import copy
+from room import Room
+from item import Item
+
+
+class Map:
+
+    rooms = {
+        'outside':  Room("Outside Cave Entrance",
+                         "North of you, the cave mount beckons."),
+
+        'foyer':    Room("Foyer",
+                         ("Dim light filters in from the south. Dusty " +
+                          "passages run north and east.")),
+
+        'overlook': Room("Grand Overlook",
+                         ("A steep cliff appears before you, falling into " +
+                          "the darkness. Ahead to the north, a light " +
+                          "flickets in the distance, but there is no " +
+                          "way across the chasm.")),
+
+        'narrow':   Room("Narrow Passage",
+                         "The narrow passage bends here from west to " +
+                         "north. The smell of gold permeates the air."),
+
+        'treasure': Room("Treasure Chamber",
+                         "You've found the long-lost treasure chamber! " +
+                         "Sadly, it has already been completely emptied " +
+                         "by earlier adventurers. The only exit is to " +
+                         "the south."),
+    }
+
+    def __init__(self):
+        self.rooms = copy.deepcopy(Map.rooms)
+
+    def setup(self):
+        self.rooms['outside'].n_to = self.rooms['foyer']
+        self.rooms['foyer'].s_to = self.rooms['outside']
+        self.rooms['foyer'].n_to = self.rooms['overlook']
+        self.rooms['foyer'].e_to = self.rooms['narrow']
+        self.rooms['overlook'].s_to = self.rooms['foyer']
+        self.rooms['narrow'].w_to = self.rooms['foyer']
+        self.rooms['narrow'].n_to = self.rooms['treasure']
+        self.rooms['treasure'].s_to = self.rooms['narrow']
+
+        self.rooms['outside'].add_item(Item("lantern", "a lantern with oil"))
+
+    def get_starting_room(self):
+        return self.rooms['outside']
